e4a28d680e0cba922a99277db42997b398542ca1
Ale-uin <alejandro@uin.cl>
2025-03-06 17:09:04 -0300

    Actualización de componentes

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,14 +1,14 @@
 // Importa componente llamado SideNav.
 import SideNav from '@/app/ui/dashboard/sidenav';
 
-
+export const experimental = true;
 /*
   Layout: Es quien comparte UI (User Interface) a través de múltiples páginas. 
   Recibe "children" como argumento, que representa el contenido 
   que se renderiza dentro del layout. El componente hijo ("children") puede ser
   una página ("page.tsx") u otro layout. Eb este caso, las carpetas dentro de
   esta carpeta, serán automáticamente anidadas a este layout.
-*/   
+*/
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
